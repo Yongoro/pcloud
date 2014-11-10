316912a90417dcf62cd74cfc3832034f1ade6517
daniel <wondeudaniel@yahoo.fr>
2014-11-10 19:11:51 +0100

    restructuration index.php

--- index.php
+++ index.php
@@ -1,60 +1,92 @@
 <?php
-// web/index.php
 
-require_once __DIR__.'/../vendor/autoload.php';
+    require_once('./pcloud/web/models/Config.php');
+    global $app; //pour utiliser la variable créée dans config.php
 
-/********************************************************************************************
-	INITIALISATION ET CONFIGURATIONS
-********************************************************************************************/
+    /*------------------------------------------------------------------------------------------------------------------------------------*/
+    //                                DECLARATION DES CLASSES RECURRENTES DE LA BIBLIOTHEQUE SILEX                                        //
+    //
+    //------------------------------------------------------------------------------------------------------------------------------------//
+    use Silex\Application;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Silex\Provider\TwigServiceProvider;
+    use Silex\Provider\SecurityServiceProvider;
+    use Silex\Provider\DoctrineServiceProvider;
+    use Silex\Provider\SessionServiceProvider;
+    use Symfony\Component\Translation\Loader\YamlFileLoader; 
+    use Silex\Provider\TranslationServiceProvider;
 
-// On définit des noms utiles
-use Silex\Application;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Silex\Provider\DoctrineServiceProvider;
+    /*--------------------------------------------------------------------------------------------------------------------------------------*/
+    //                                                                                                                                      //
+    //                                               DEBUT DES ROUTES                                                                       //
+    //                                                                                                                                      //
+    /*--------------------------------------------------------------------------------------------------------------------------------------*/
 
-$app = new Silex\Application();
-
-//to ease debugging
-$app['debug'] = true;
-
-//enregistrement au langage de templating TWIG et précision de l'emplacement des templates
-$app->register(new Silex\Provider\TwigServiceProvider(), 
-               array('twig.path' => './pcloud/views',));
-
-//souscription du service des sessions
-$app->register(new Silex\Provider\SessionServiceProvider());
-
-//configuration acces à la BD
-$app->register(new Silex\Provider\DoctrineServiceProvider(),
-  array('db.options' => array(
-        'driver'   => 'pdo_mysql',
-        'host'     => 'localhost',  
-        'user'     => 'root',  
-        'password' => '',
-        'charset'  => 'utf8',
-        'dbname'   => 'pcloud' 
-)));
-
-/***************************************************************************************
-	DEFINITIONS DES ROUTES
-***************************************************************************************/
+    // On définit une route pour l'url /
+    $app->get('/', function(Application $app) {
+       
+        return $app['twig']->render('/pcloud/web/views/accueil.twig',array('confirmText' => "", 'confirmTextLogin'=>""));
+        //return $app->redirect('/accueil');
+        //return $app['twig']->render('/pcloud/web/views/admin/admin.php', array('nom' => $n));
+    });
 
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
+
+    // pour la demnde d'inscription
+    $app->match('/accueil', function(Application $app, Request $req){
+
+    $msg="";
+    
+    // recuperation données du formulaire
+    
+    $pseudo=$req->request->get('pseudo');
+    $name=$req->request->get('name'); 
+    $surname=$req->request->get('surname');     
+    $pass1= sha1($req->request->get('pass1'));
+    $pass2= sha1($req->request->get('pass2'));
+    $email1=$req->request->get('mail1');
+    $email2=$req->request->get('mail2'); 
+    //$gender=$req->request->get('gender');  
+
+
+    if( isset($pseudo) && isset($name) && isset($surname) && isset($pass1) && isset($email1) ){
+    
+        //on teste si les données sont cohérentes
+        if(strcmp($pass1, $pass2)==0  && strcmp($email1, $email2)==0){
+
+          // on controle si le pseudo est dejà utilisé
+          
+
+          // ICI ON VA INSERER LES DONNEES DANS LA BD,  appel de la procedure
+          
+          $q=$app['db']->prepare('CALL procedure_user_request(?,?,?,?,?,?,?)');
+          $q->execute(array($pseudo,$pass1,$email1,1,'datetime',$name,$surname));
+
+          
+           //inscription faite          
+          $msg="subscription received";          
+        }
+        else{
+
+          //donnees incoherentes          
+          $msg="mots de passe ou mails ne correspondent pas";
+        }
+      
+    }  
+  
+    return $app['twig']->render('/pcloud/web/views/accueil.twig', array('confirmText' => $msg, 'confirmTextLogin'=>""));
 });
 
 
-//exemple utilisation de twig et binding de la route avec un certain nom
-$app->get('/', function(Application $app) {
-    return $app['twig']->render('admin.php', array(
-        'nom' => 'NDIAYE'
-    ));
-})->bind('homepage');
+// route pour le login des utilisateurs
+$app->match('/login', function(Application $app, Request $req){
 
+$msg = "welcome user";
+  return $app['twig']->render('/pcloud/web/views/user/user.twig', array('confirmText' => $msg));
+});
 
-//dernière instruction du programme
+// On lance l'application
 $app->run();
 
 ?>
